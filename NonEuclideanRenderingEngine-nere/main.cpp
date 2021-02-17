6620f5907f32f8bf4fbc4bf6c6b6dff502110867
kobihall <kobihallacc@gmail.com>
2021-02-17 12:25:24 -0800

    Initial Commit

---
+++ NonEuclideanRenderingEngine-nere/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NonEuclideanRenderingEngine-nere
+//
+//  Created by Kobi Hall on 2/17/21.
+//  Copyright © 2021 Kobi Hall. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
